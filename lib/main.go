b2a224b8a17515a9b7e64356d546c53a0fd9f91c
Tarun Chinmai Sekar <tsekar@akamai.com>
2020-04-17 20:27:54 -0700

    Include lengths

--- lib/main.go
+++ lib/main.go
@@ -37,15 +37,31 @@ func parseBucketHeader(bucket *stack.Bucket, multipleBuckets bool) string {
 	return fmt.Sprintf("%d: %s%s\n", len(bucket.IDs), bucket.State, extra)
 }
 
-func stackLines(signature *stack.Signature) string {
+func stackLines(signature *stack.Signature, srcLen, pkgLen int) string {
 	out := make([]string, len(signature.Stack.Calls))
 	for i, line := range signature.Stack.Calls {
-		out[i] = fmt.Sprintf("%s %s %s(%s)", line.Func.PkgName(), formatCall(&line), line.Func.Name(), &line.Args)
+		out[i] = fmt.Sprintf("%s%-*s %s%-*s %s%s%s(%s)", pkgLen, line.Func.PkgName(), srcLen, formatCall(&line), line.Func.Name(), &line.Args)
 	}
 	if signature.Stack.Elided {
 		out = append(out, "    (...)")
 	}
 	return strings.Join(out, "\n") + "\n"
+}
+
+func calcLengths(buckets []*stack.Bucket) (int, int) {
+	srcLen := 0
+	pkgLen := 0
+	for _, bucket := range buckets {
+		for _, line := range bucket.Signature.Stack.Calls {
+			if l := len(formatCall(&line)); l > srcLen {
+				srcLen = l
+			}
+			if l := len(line.Func.PkgName()); l > pkgLen {
+				pkgLen = l
+			}
+		}
+	}
+	return srcLen, pkgLen
 }
 
 func ParsePanicString(stackTrace string, guessPaths bool) (string, error) {
@@ -68,10 +84,11 @@ func ParsePanicString(stackTrace string, guessPaths bool) (string, error) {
 	buckets := stack.Aggregate(ctx.Goroutines, stack.AnyPointer)
 	multipleBuckets := len(buckets) > 1
 
+	srcLen, pkgLen := calcLengths(buckets)
 	for _, bucket := range buckets {
 		header := parseBucketHeader(bucket, multipleBuckets)
 		_, _ = io.WriteString(writer, header)
-		_, _ = io.WriteString(writer, stackLines(&bucket.Signature))
+		_, _ = io.WriteString(writer, stackLines(&bucket.Signature, srcLen, pkgLen))
 	}
 
 	return compressedString.String(), nil
