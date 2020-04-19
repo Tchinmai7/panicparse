714fecbe0d034952f9e4c67ea2b850c0c60f008d
Tarun Chinmai Sekar <tsekar@akamai.com>
2020-04-18 19:42:12 -0700

    Include length calculations

--- lib/main.go
+++ lib/main.go
@@ -3,6 +3,7 @@ package lib
 import (
 	"bufio"
 	"bytes"
+	"errors"
 	"fmt"
 	"io"
 	"strings"
@@ -40,7 +41,7 @@ func parseBucketHeader(bucket *stack.Bucket, multipleBuckets bool) string {
 func stackLines(signature *stack.Signature, srcLen, pkgLen int) string {
 	out := make([]string, len(signature.Stack.Calls))
 	for i, line := range signature.Stack.Calls {
-		out[i] = fmt.Sprintf("%s%-*s %s%-*s %s%s%s(%s)", pkgLen, line.Func.PkgName(), srcLen, formatCall(&line), line.Func.Name(), &line.Args)
+		out[i] = fmt.Sprintf("%-*s %-*s %s(%s)", pkgLen, line.Func.PkgName(), srcLen, formatCall(&line), line.Func.Name(), &line.Args)
 	}
 	if signature.Stack.Elided {
 		out = append(out, "    (...)")
@@ -64,20 +65,20 @@ func calcLengths(buckets []*stack.Bucket) (int, int) {
 	return srcLen, pkgLen
 }
 
-func ParsePanicString(stackTrace string, guessPaths bool) (string, error) {
+func ParsePanicString(stackTrace string) (string, error) {
 	r := strings.NewReader(stackTrace)
 	fmt.Printf(stackTrace)
 	var compressedString bytes.Buffer
 	writer := bufio.NewWriter(&compressedString)
 
 	//writer would contain Junk after ParseDump
-	ctx, err := stack.ParseDump(r, writer, guessPaths)
+	ctx, err := stack.ParseDump(r, writer, false)
 	if err != nil {
-		return " ", err
+		return "", err
 	}
 
 	if ctx == nil {
-		return "", err
+		return "", errors.New("ctx is null")
 	}
 
 	compressedString.Reset()
