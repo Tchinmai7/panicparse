c35852f22bcab68a3c31cbdaee5149adb10dce4d
Tarun Chinmai Sekar <tsekar@akamai.com>
2020-04-22 17:01:35 -0700

    Remove code not used

--- cmd/panic/internal/incorrect/correct.go
+++
@@ -1,8 +0,0 @@
-// Package correct is in directory incorrect. If the call stack is
-// incorrect.Panic(), you know the parsing failed.
-package correct
-
-// Panic panics.
-func Panic() {
-	panic(42)
-}
